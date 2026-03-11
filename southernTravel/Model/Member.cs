abe256dea96d7246b38bb21ca8a562da2abf4b9f
dongyuling <dongyuling@semi-tech.com>
2026-03-11 15:33:10 +0800

    調整會員註冊生日欄位驗證與型別
    強化會員註冊時生日欄位的格式驗證，將 Member.Birthday 型別由 DateTime? 改為 string，並於 Controller 及 Validator 層級加入嚴格的日期格式與合理性檢查，避免不正確的生日資料進入系統。更新相關資料庫檔案。

--- southernTravel/Model/Member.cs
+++ southernTravel/Model/Member.cs
@@ -56,6 +56,6 @@ namespace southernTravel.Model
         public string PhoneNumber { get; set; } = null!;
 
         [Required]
-        public DateTime? Birthday { get; set; }
+        public string Birthday { get; set; } = null!;
     }
 }
