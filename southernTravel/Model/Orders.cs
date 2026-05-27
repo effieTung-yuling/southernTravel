2207cc5d2b30805cab947a81e4e2a939c3e86f5d
dongyuling <dongyuling@semi-tech.com>
2026-05-27 16:35:08 +0800

    新增 Orders 類別至 southernTravel.Model 命名空間
    新增一個名為 Orders 的類別至 southernTravel.Model 命名空間。
    該類別包含 Id、OrderNo 和 Name 三個屬性，分別對應
    long 和 string 類型，並為字串屬性設定了預設值。

---
+++ southernTravel/Model/Orders.cs
@@ -0,0 +1,11 @@
+﻿namespace southernTravel.Model
+{
+    public class Orders
+    {
+        public long Id { get; set; }
+
+        public string OrderNo { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
